3c28b92d53acbb3257855b41126ca7d3688e63c7
Travis French <travster217@gmail.com>
2024-07-11 13:49:29 -0500

    adding crud operations

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { connectToDb, getDb } = require('./server')
 
 // init app
 const app = express()
+app.use(express.json())
 
 // db connection
 let db
@@ -20,7 +21,7 @@ connectToDb((err) => {
 
 //routes
 app.get('/list', (req, res) => {
-    let todoItems = []
+    const todoItems = []
 
     db.collection('todoappcollection')
         .find()
@@ -30,17 +31,68 @@ app.get('/list', (req, res) => {
             res.status(200).json(todoItems)
         })
         .catch(() => {
-            res.status(500).json({error: 'Could not fetch documents'})
+            res.status(500).json({error: 'Could not fetch list'})
         })
 })
 
 app.get('/list/:id', (req, res) => {
+
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection('todoappcollection')
+            .findOne({_id: new ObjectId(req.params.id)})
+            .then(doc => {
+                res.status(200).json(doc)
+            })
+            .catch(err => {
+                res.status(500).json({error: 'Could not fetch the list item'})
+            })
+    } else {
+        res.status(500).json({error: 'not a valid list id'})
+    }
+
+})
+
+app.post('/list', (req, res) => {
+    const listItem = req.body
+
     db.collection('todoappcollection')
-    .findOne({_id: new ObjectId(req.params.id)})
-    .then(doc => {
-        res.status(200).json(doc)
-    })
-    .catch(err => {
-        res.status(500).json({error: 'Could not fetch the documents'})
-    })
+        .insertOne(listItem)
+        .then((result) => {
+            res.status(201).json(result)
+        })
+        .catch(err => {
+            res.status(500).json({ err: 'could not create new list item'})
+        })
+})
+
+app.delete('/list/:id', (req, res) => {
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection('todoappcollection')
+            .deleteOne({_id: new ObjectId(req.params.id)})
+            .then(result => {
+                res.status(200).json(result)
+            })
+            .catch(err => {
+                res.status(500).json({error: 'Could not delete the list item'})
+            })
+    } else {
+        res.status(500).json({error: 'not a valid list id'})
+    }
+})
+
+app.patch('/list/:id', (req, res) => {
+    const updates = req.body
+
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection('todoappcollection')
+            .updateOne({_id: new ObjectId(req.params.id)}, {$set: updates})
+            .then(result => {
+                res.status(200).json(result)
+            })
+            .catch(err => {
+                res.status(500).json({error: 'Could not update the list item'})
+            })
+    } else {
+        res.status(500).json({error: 'not a valid list id'})
+    }
 })
